d2164052ceb9702043a2ca2da21ad99023852cd0
Alicia Petgrave <alicia.petgrave@gmail.com>
2019-12-18 00:25:16 -0500

    25 passing

--- index.js
+++ index.js
@@ -264,9 +264,9 @@ function tallyUpDonations(runners) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
-  function counter() {
-    ++count
+  let count = 0;
+  return function counter() {
+    return count++
   }
   // BROKEN CODE ENDS
 }
@@ -291,9 +291,15 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
-}
+function counterMakerWithLimit(limit) {
+  let count = -1
+  return function(){
+    if (count === limit){
+      count = -1 
+    }
+    return ++count 
+  }
+} 
 
 /////////////// END OF CHALLENGE ///////////////
 /////////////// END OF CHALLENGE ///////////////
